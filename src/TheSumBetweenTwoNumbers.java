795d82de965830d847b132b976f609780188af00
Saad E <21321555+se447@users.noreply.github.com>
2019-03-19 20:36:18 -0400

    Defined java class

---
+++ src/TheSumBetweenTwoNumbers.java
@@ -0,0 +1,14 @@
+/*
+Sum of set of numbers, the program should ask for both the lower and upper bound. 
+You can assume that the users first gives the smaller number and then the greater number.
+*/
+import java.util.Scanner;
+
+
+public class TheSumBetweenTwoNumbers {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        
+    }
+}
